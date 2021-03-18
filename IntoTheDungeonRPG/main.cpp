7fe9d587d82dca8f4d1c3888da42a605cc8eddab
poolpal <zoo5871@naver.com>
2021-03-18 22:25:30 +0900

    초기 커밋.

---
+++ IntoTheDungeonRPG/main.cpp
@@ -0,0 +1,200 @@
+#include<iostream>
+#include <random>
+#include<vector>
+#include <algorithm>
+#include <Windows.h>
+#include<iomanip>
+
+using namespace std;
+
+int Random_Value(int min, int max) {
+
+	random_device rd;
+	mt19937_64 gen(rd());
+	uniform_int_distribution<int> dis(min, max);
+
+	return dis(gen);
+}
+
+enum MAIN_MENU {
+	MM_NONE = 0,
+	MM_MAP,
+	MM_STORE,
+	MM_INVENTORY,
+	MM_EXIT
+};
+
+enum MAP_TYPE {
+	MT_NONE = 0,
+	MT_EASY,
+	MT_NORMAL,
+	MT_HARD,
+	MT_BACK
+};
+
+void Logo() {
+	cout << "==============================================================================================================" << endl;
+	cout << " _____   _   _   _____   _____   _____   _   _   _____   _____   _   _   _   _   _____   _____   _____   _   _" << endl;
+	cout << "|_   _| | \\ | | |_   _| |  _  | |_   _| | | | | |  ___| |  _  \\\ | | | | | \\\ | | |  __ \\\ | ___| |  _  | | \\\ | |" << endl;
+	cout << "  | |   |  \\| |   | |   | | | |   | |   | |_| | | |__   | | | | | | | | |  \\\| | | |  \\\/ | |__  | | | | |  \\\| |" << endl;
+	cout << "  | |   | . ` |   | |   | | | |   | |   |  _  | |  __|  | | | | | | | | | . ` | | | __  |  __| | | | | | . ` |" << endl;
+	cout << "  | |_  | |\\  |   | |   \\ \\_/ /   | |   | | | | | |___  | |/ /  | |_| | | |\\\  | | |_\\\ \\\ | |___ \\\ \\\_/ / | | \\\ |" << endl;
+	cout << " \\___/  \\_| \\_/   \\_/    \\___/    \\_/   \\_| |_/ \\____/  |___/    \\___/  \\_| \\_/  \\____/ \\____/  \\___/  \\_| \\_/" << endl;
+	cout << endl;
+	cout << "==============================================================================================================" << endl;
+}
+
+void MainPic() {
+	const char pic[] =
+		"                    ..                                              \n\r"
+		"                      ++`                                            \n\r"
+		"                       :ho.        `.-/++/.                          \n\r"
+		"                        `/hh+.         ``:sds:                       \n\r"
+		"                          `-odds/-`        .MNd/`                    \n\r"
+		"                             `.+ydmdyo/:--/yMMMMd/                   \n\r"
+		"                                `:+hMMMNNNMMMddNMMh:`                \n\r"
+		"                   `-:/+++/:-:ohmNMMMMMMMMMMMm+-+mMNd`               \n\r"
+		"                `-+oo+osdMMMNMMMMMMMMMMMMMMMMMMNmNMMM/`              \n\r"
+		"                ```   .+mMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmho:.`         \n\r"
+		"                    `omMMMMMMMMMMMMMMMMMMNMdydMMdNMMMMMMMMdo+-       \n\r"
+		"                .:oymMMMMMMMMMMMMMNdo/hMMd+ds-:h/-yMdydMNdNdNN+      \n\r"
+		"              -oosdMMMMMMMMMMMMMMd:`  `yMM+.+h+.-  /y `/m.:mmmN      \n\r"
+		"             -:`  dMMMMMMMMMMMMMd.     `mMNo..+y/`  .   .  -/.s      \n\r"
+		"             `   -MMMMMMMMMMMMMM-       -mMMmo-./s/.`         `      \n\r"
+		"                `+MMMMMMMMMMMMMM-        .smMy:.``-+oo+//:-.`        \n\r"
+		"               .yNMMMMMMMMMMMMMMd.         .+dmh+:.  `-::/+:.        \n\r"
+		"               y+-mMMMMMMMMMMMMMMm/`          ./o+-`       .         \n\r"
+		"              :-  :MMMMMMMMMMMMMMMMmy/.`                             \n\r"
+		"              `   `hMMMMMMMMMMMMMMMMMMNds/.`                         \n\r"
+		"                  sNhNMMMMMMMMMMMMMMMMMMMMNh+.                       \n\r"
+		"                 -d. :mMMMMMMMMMMMMMMMMMMMMMMNh:`                    \n\r"
+		"                 /.   .hMMMMMMMMMMMMMMMMMMMMMMMMh.                   \n\r"
+		"                 .     `sMMMMMMMMMMMMMMMMMMMMMMMMN.                  \n\r"
+		"                         hMMMMMMMMMMMMMMMMMMMMMMMMy                  \n\r"
+		"                         +MMMMMMMMMMMMMMMMMMMMMMMMh                      ";
+
+	cout <<setw(100)<<pic << endl;
+}
+
+int main() {
+	system(" mode  con lines=50   cols=110 ");
+
+	//로비 생성
+
+	//const char msgWelcome[] =
+	//    "              *******************************************************\n\r"
+	//    "              *                                                     *\n\r"
+	//    "              *                   IntoTheDungeon                    *\n\r"
+	//    "              *                                                     *\n\r"
+	//    "              *******************************************************\n\r";
+
+	while (1) {
+		system("cls");
+		Logo();
+		MainPic();
+		cout.setf(ios::left);
+		cout << "1. 게임 시작" << endl;
+		cout << "2. 게임 종료" << endl;
+		cout << "메뉴를 입력해주세요 : ";
+		int iMenu;
+
+		cin >> iMenu;
+
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(1024, '\n');
+			continue;
+		}
+
+		if (iMenu == 2) {
+			break;
+		}
+
+		if (iMenu == 1) {
+			while (1) {
+
+				system("cls");
+				Logo();
+
+				cout << "0. 튜토리얼" << endl;
+				cout << "1. 맵" << endl;
+				cout << "2. 상점" << endl;
+				cout << "3. 가방" << endl;
+				cout << "4. 시작메뉴로" << endl;
+				cout << "메뉴를 선택하세요 : ";
+				cin >> iMenu;
+
+				if (cin.fail()) {
+					cin.clear();
+					cin.ignore(1024, '\n');
+					continue;
+				}
+
+				if (iMenu == MM_EXIT) {
+					break;
+				}
+
+				switch (iMenu)
+				{
+				case MM_NONE:
+					while (1) {
+						system("cls");
+						Logo();
+						cout << "튜토리얼에 오신걸 환영합니다!" << endl;
+
+						cout << "1. 게임 진행" << endl;
+						cout << "2. 전투" << endl;
+						cout << "3. 아이템" << endl;
+						cout << "4. 뒤로가기" << endl;
+						cout << "번호를 선택하세요 : ";
+
+						cin >> iMenu;
+						if (cin.fail()) {
+							cin.clear();
+							cin.ignore(1024, '\n');
+							continue;
+						}
+
+						if (iMenu == MT_BACK) {
+							break;
+						}
+					}
+					break;
+				case MM_MAP:
+					while (1) {
+						system("cls");
+						Logo();
+						cout << "1. 쉬움" << endl;
+						cout << "2. 보통" << endl;
+						cout << "3. 어려움" << endl;
+						cout << "4. 뒤로가기" << endl;
+						cout << "맵을 선택하세요 : ";
+						cin >> iMenu;
+
+						if (cin.fail()) {
+							cin.clear();
+							cin.ignore(1024, '\n');
+							continue;
+						}
+
+						if (iMenu == MT_BACK) {
+							break;
+						}
+					}
+
+					break;
+				case MM_STORE:
+					break;
+				case MM_INVENTORY:
+					break;
+				default:
+					cout << "잘못 선택하였습니다." << endl;
+					break;
+				}
+			}
+		}
+	}
+
+
+	return 0;
+}

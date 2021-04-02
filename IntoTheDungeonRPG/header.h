f48171fb9231838f09374e5ca2399fdce3854e42
poolpal <zoo5871@naver.com>
2021-04-02 22:48:29 +0900

    Add Func : Equip and UnEquip

--- IntoTheDungeonRPG/header.h
+++ IntoTheDungeonRPG/header.h
@@ -1,67 +1,5 @@
 #pragma once
 
-#include<iostream>
-#include <random>
-#include<vector>
-#include <algorithm>
-#include <Windows.h>
-#include<iomanip>
-#include<fstream>
-#include<string>
-
-void Logo() {
-	cout << "==============================================================================================================" << endl;
-	cout << " _____   _   _   _____   _____   _____   _   _   _____   _____   _   _   _   _   _____   _____   _____   _   _" << endl;
-	cout << "|_   _| | \\ | | |_   _| |  _  | |_   _| | | | | |  ___| |  _  \\\ | | | | | \\\ | | |  __ \\\ | ___| |  _  | | \\\ | |" << endl;
-	cout << "  | |   |  \\| |   | |   | | | |   | |   | |_| | | |__   | | | | | | | | |  \\\| | | |  \\\/ | |__  | | | | |  \\\| |" << endl;
-	cout << "  | |   | . ` |   | |   | | | |   | |   |  _  | |  __|  | | | | | | | | | . ` | | | __  |  __| | | | | | . ` |" << endl;
-	cout << "  | |_  | |\\  |   | |   \\ \\_/ /   | |   | | | | | |___  | |/ /  | |_| | | |\\\  | | |_\\\ \\\ | |___ \\\ \\\_/ / | | \\\ |" << endl;
-	cout << " \\___/  \\_| \\_/   \\_/    \\___/    \\_/   \\_| |_/ \\____/  |___/    \\___/  \\_| \\_/  \\____/ \\____/  \\___/  \\_| \\_/" << endl;
-	cout << endl;
-	cout << "==============================================================================================================" << endl;
-}
-
-void MainPic() {
-	const char pic[] =
-		"                    ..                                              \n\r"
-		"                      ++`                                            \n\r"
-		"                       :ho.        `.-/++/.                          \n\r"
-		"                        `/hh+.         ``:sds:                       \n\r"
-		"                          `-odds/-`        .MNd/`                    \n\r"
-		"                             `.+ydmdyo/:--/yMMMMd/                   \n\r"
-		"                                `:+hMMMNNNMMMddNMMh:`                \n\r"
-		"                   `-:/+++/:-:ohmNMMMMMMMMMMMm+-+mMNd`               \n\r"
-		"                `-+oo+osdMMMNMMMMMMMMMMMMMMMMMMNmNMMM/`              \n\r"
-		"                ```   .+mMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmho:.`         \n\r"
-		"                    `omMMMMMMMMMMMMMMMMMMNMdydMMdNMMMMMMMMdo+-       \n\r"
-		"                .:oymMMMMMMMMMMMMMNdo/hMMd+ds-:h/-yMdydMNdNdNN+      \n\r"
-		"              -oosdMMMMMMMMMMMMMMd:`  `yMM+.+h+.-  /y `/m.:mmmN      \n\r"
-		"             -:`  dMMMMMMMMMMMMMd.     `mMNo..+y/`  .   .  -/.s      \n\r"
-		"             `   -MMMMMMMMMMMMMM-       -mMMmo-./s/.`         `      \n\r"
-		"                `+MMMMMMMMMMMMMM-        .smMy:.``-+oo+//:-.`        \n\r"
-		"               .yNMMMMMMMMMMMMMMd.         .+dmh+:.  `-::/+:.        \n\r"
-		"               y+-mMMMMMMMMMMMMMMm/`          ./o+-`       .         \n\r"
-		"              :-  :MMMMMMMMMMMMMMMMmy/.`                             \n\r"
-		"              `   `hMMMMMMMMMMMMMMMMMMNds/.`                         \n\r"
-		"                  sNhNMMMMMMMMMMMMMMMMMMMMNh+.                       \n\r"
-		"                 -d. :mMMMMMMMMMMMMMMMMMMMMMMNh:`                    \n\r"
-		"                 /.   .hMMMMMMMMMMMMMMMMMMMMMMMMh.                   \n\r"
-		"                 .     `sMMMMMMMMMMMMMMMMMMMMMMMMN.                  \n\r"
-		"                         hMMMMMMMMMMMMMMMMMMMMMMMMy                  \n\r"
-		"                         +MMMMMMMMMMMMMMMMMMMMMMMMh                      ";
-
-	cout << setw(100) << pic << endl;
-}
-
-
-int Random_Value(int min, int max) {
-
-	random_device rd;
-	mt19937_64 gen(rd());
-	uniform_int_distribution<int> dis(min, max);
-
-	return dis(gen);
-}
 
 enum MAIN_MENU {
 	MM_NONE = 0,
@@ -104,30 +42,14 @@ enum ITEM_TYPE {
 	IT_BACK,
 };
 
-struct _tagItem {
-
-	string strName;
-	string strTypeName;
-
-	ITEM_TYPE eType;
-	int iMin;
-	int iMax;
-	int iPrice;
-	int iSell;
-
-	string Description;
-};
 
 #define MAX_INVENTORY 20
 #define STORE_ARMOR_MAX 3
 #define STORE_WEAPON_MAX 3
+#define WEAPON_MAX 20
+#define ARMOR_MAX 20
+#define MAX_EQUIPPED 2
 
-struct _tagInvcentory
-{
-	_tagItem tItems[MAX_INVENTORY];
-	int iGold;
-	int iItemCount = 0;
-};
 
 enum STORE_MENU {
 	SM_NONE = 0,
@@ -139,51 +61,6 @@ enum STORE_MENU {
 enum IN_MENU {
 	IN_NONE = 0,
 	IN_EQUIP,
+	IN_UNEQUIP,
 	IN_BACK,
-};
-
-struct _tagPlayer
-{
-	string strName;
-	string JobName;
-	JOB ejob;
-
-	int iAttackMin;
-	int iAttackMax;
-
-	int iArmorMin;
-	int iArmorMax;
-
-	int iHP;
-	int iHPMax;
-	int iMP;
-	int iMPMax;
-
-	int iExp;
-	int iLevel;
-
-	_tagInvcentory iInventory;
-};
-
-struct _tagMonster
-{
-	string strName;
-
-	int iAttackMin;
-	int iAttackMax;
-
-	int iArmorMin;
-	int iArmorMax;
-
-	int iHP;
-	int iHPMax;
-	int iMP;
-	int iMPMax;
-
-	int iLevel;
-
-	int iExp;
-
-	int iGoldMin;
-	int iGoldMax;
 };
--- IntoTheDungeonRPG/main.cpp
+++ IntoTheDungeonRPG/main.cpp
@@ -10,19 +10,146 @@
 
 using namespace std;
 
+struct _tagItem {
 
-void Show_Status(_tagPlayer tPlayer) {
+	string strName;
+	string strTypeName;
+
+	ITEM_TYPE eType;
+	int iMin;
+	int iMax;
+	int iPrice;
+	int iSell;
+
+	string Description;
+};
+
+struct _tagInvcentory
+{
+	_tagItem tItems[MAX_INVENTORY];
+	int iGold;
+	int iItemCount = 0;
+};
+
+struct _tagPlayer
+{
+	string strName;
+	string JobName;
+	JOB ejob;
+
+	int iAttackMin;
+	int iAttackMax;
+
+	int iArmorMin;
+	int iArmorMax;
+
+	int iHP;
+	int iHPMax;
+	int iMP;
+	int iMPMax;
+
+	int iExp;
+	int iLevel;
+
+	_tagInvcentory iInventory;
+
+	_tagItem *iEquipped[MAX_EQUIPPED];
+};
+
+
+struct _tagMonster
+{
+	string strName;
+
+	int iAttackMin;
+	int iAttackMax;
+
+	int iArmorMin;
+	int iArmorMax;
+
+	int iHP;
+	int iHPMax;
+	int iMP;
+	int iMPMax;
+
+	int iLevel;
+
+	int iExp;
+
+	int iGoldMin;
+	int iGoldMax;
+};
+
+
+void Logo() {
+	cout << "==============================================================================================================" << endl;
+	cout << " _____   _   _   _____   _____   _____   _   _   _____   _____   _   _   _   _   _____   _____   _____   _   _" << endl;
+	cout << "|_   _| | \\ | | |_   _| |  _  | |_   _| | | | | |  ___| |  _  \\\ | | | | | \\\ | | |  __ \\\ | ___| |  _  | | \\\ | |" << endl;
+	cout << "  | |   |  \\| |   | |   | | | |   | |   | |_| | | |__   | | | | | | | | |  \\\| | | |  \\\/ | |__  | | | | |  \\\| |" << endl;
+	cout << "  | |   | . ` |   | |   | | | |   | |   |  _  | |  __|  | | | | | | | | | . ` | | | __  |  __| | | | | | . ` |" << endl;
+	cout << "  | |_  | |\\  |   | |   \\ \\_/ /   | |   | | | | | |___  | |/ /  | |_| | | |\\\  | | |_\\\ \\\ | |___ \\\ \\\_/ / | | \\\ |" << endl;
+	cout << " \\___/  \\_| \\_/   \\_/    \\___/    \\_/   \\_| |_/ \\____/  |___/    \\___/  \\_| \\_/  \\____/ \\____/  \\___/  \\_| \\_/" << endl;
+	cout << endl;
+	cout << "==============================================================================================================" << endl;
+}
+
+void MainPic() {
+	const char pic[] =
+		"                    ..                                              \n\r"
+		"                      ++`                                            \n\r"
+		"                       :ho.        `.-/++/.                          \n\r"
+		"                        `/hh+.         ``:sds:                       \n\r"
+		"                          `-odds/-`        .MNd/`                    \n\r"
+		"                             `.+ydmdyo/:--/yMMMMd/                   \n\r"
+		"                                `:+hMMMNNNMMMddNMMh:`                \n\r"
+		"                   `-:/+++/:-:ohmNMMMMMMMMMMMm+-+mMNd`               \n\r"
+		"                `-+oo+osdMMMNMMMMMMMMMMMMMMMMMMNmNMMM/`              \n\r"
+		"                ```   .+mMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmho:.`         \n\r"
+		"                    `omMMMMMMMMMMMMMMMMMMNMdydMMdNMMMMMMMMdo+-       \n\r"
+		"                .:oymMMMMMMMMMMMMMNdo/hMMd+ds-:h/-yMdydMNdNdNN+      \n\r"
+		"              -oosdMMMMMMMMMMMMMMd:`  `yMM+.+h+.-  /y `/m.:mmmN      \n\r"
+		"             -:`  dMMMMMMMMMMMMMd.     `mMNo..+y/`  .   .  -/.s      \n\r"
+		"             `   -MMMMMMMMMMMMMM-       -mMMmo-./s/.`         `      \n\r"
+		"                `+MMMMMMMMMMMMMM-        .smMy:.``-+oo+//:-.`        \n\r"
+		"               .yNMMMMMMMMMMMMMMd.         .+dmh+:.  `-::/+:.        \n\r"
+		"               y+-mMMMMMMMMMMMMMMm/`          ./o+-`       .         \n\r"
+		"              :-  :MMMMMMMMMMMMMMMMmy/.`                             \n\r"
+		"              `   `hMMMMMMMMMMMMMMMMMMNds/.`                         \n\r"
+		"                  sNhNMMMMMMMMMMMMMMMMMMMMNh+.                       \n\r"
+		"                 -d. :mMMMMMMMMMMMMMMMMMMMMMMNh:`                    \n\r"
+		"                 /.   .hMMMMMMMMMMMMMMMMMMMMMMMMh.                   \n\r"
+		"                 .     `sMMMMMMMMMMMMMMMMMMMMMMMMN.                  \n\r"
+		"                         hMMMMMMMMMMMMMMMMMMMMMMMMy                  \n\r"
+		"                         +MMMMMMMMMMMMMMMMMMMMMMMMh                      ";
+
+	cout << setw(100) << pic << endl;
+}
+
+
+int Random_Value(int min, int max) {
+
+	random_device rd;
+	mt19937_64 gen(rd());
+	uniform_int_distribution<int> dis(min, max);
+
+	return dis(gen);
+}
+
+
+void Show_Status(_tagPlayer *tPlayer) {
 	while (1) {
 		system("cls");
 		Logo();
 		int iMenu = 0;
-		cout << tPlayer.strName << "의 스텟" << endl;
-		cout << "HP :" << "[" << tPlayer.iHP << "/" << tPlayer.iHPMax << "]" << endl;
-		cout << "MP :" << "[" << tPlayer.iMP << "/" << tPlayer.iMPMax << "]" << endl;
-		cout << "공격력 :" << "[" << tPlayer.iAttackMin << "-" << tPlayer.iAttackMax << "]" << endl;
-		cout << "방어력 :" << "[" << tPlayer.iArmorMin << "-" << tPlayer.iArmorMax << "]" << endl;
-		cout << "EXP :" << "[" << tPlayer.iExp << "]" << endl;
-		cout << "Gold : " << tPlayer.iInventory.iGold << "G" << endl;
+		cout << tPlayer->strName << "의 스텟" << endl;
+		cout << "HP :" << "[" << tPlayer->iHP << "/" << tPlayer->iHPMax << "]" << endl;
+		cout << "MP :" << "[" << tPlayer->iMP << "/" << tPlayer->iMPMax << "]" << endl;
+		cout << "공격력 :" << "[" << tPlayer->iAttackMin << "-" << tPlayer->iAttackMax << "]" << endl;
+		cout << "방어력 :" << "[" << tPlayer->iArmorMin << "-" << tPlayer->iArmorMax << "]" << endl;
+		cout << "EXP :" << "[" << tPlayer->iExp << "]" << endl;
+		cout << "Gold : " << tPlayer->iInventory.iGold << "G" << endl;
+		cout << "무기 : " << tPlayer->iEquipped[0]->strName << endl;
+		cout << "방어구 : " << tPlayer->iEquipped[1]->strName << endl;
 		cout << "번호를 입력해주세요 [뒤로가기는 0번] : ";
 		cin >> iMenu;
 		if (cin.fail()) {
@@ -275,6 +402,219 @@ void SetStoreItems(_tagItem *tStoreWeapon, _tagItem *tStoreArmor) {
 	tStoreArmor[2].iSell = tStoreArmor[2].iPrice * 0.8f;
 }
 
+void Menu_Equip(_tagPlayer* tPlayer) {
+	
+
+	int iMenu = 0;
+	while (1) {
+		//장비를 보여주고 장착하는 기능 추가
+		system("cls");
+		Logo();
+
+		_tagItem *weapons[WEAPON_MAX] = {};
+		int weapon_count = 0;
+		_tagItem *armors[ARMOR_MAX] = {};
+		int armor_count = 0;
+
+		cout << endl;
+		cout << "--------------------무기-------------------" << endl;
+		cout << endl;
+
+		for (int i = 0; i < tPlayer->iInventory.iItemCount; i++) {
+			if (tPlayer->iInventory.tItems[i].eType == IT_WEAPON) {
+				weapons[weapon_count] = &tPlayer->iInventory.tItems[i];
+				weapon_count++;
+				cout << "-----------------------------------------------" << endl;
+				cout << " " << "[" << weapon_count << setw(2) << "]" << setw(15) << tPlayer->iInventory.tItems[i].strName << setw(6) << "|" << setw(8) << tPlayer->iInventory.tItems[i].strTypeName << setw(5) << "|" << tPlayer->iInventory.tItems[i].iMin << "-" << tPlayer->iInventory.tItems[i].iMax << endl;
+			}
+		}
+
+		cout << endl;
+		cout << endl;
+		cout << "--------------------방어구-------------------" << endl;
+		cout << endl;
+
+		for (int i = 0; i < tPlayer->iInventory.iItemCount; i++) {
+			if (tPlayer->iInventory.tItems[i].eType == IT_ARMOR) {
+				armors[armor_count]= &tPlayer->iInventory.tItems[i];
+				armor_count++;
+				cout << "-----------------------------------------------" << endl;
+				cout << " " << "[" << armor_count << setw(2) << "]" << setw(15) << tPlayer->iInventory.tItems[i].strName << setw(6) << "|" << setw(8) << tPlayer->iInventory.tItems[i].strTypeName << setw(5) << "|" << tPlayer->iInventory.tItems[i].iMin << "-" << tPlayer->iInventory.tItems[i].iMax << endl;
+			}
+		}
+
+		cout << endl;
+
+		cout << "1. 무기"<<endl;
+		cout << "2. 방어구"<<endl;
+		cout << endl;
+		cout << "장착할 아이템의 종류를 고르세요 [뒤로 가기는 0번] :";
+
+		cin >> iMenu;
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(1024, '\n');
+			continue;
+		}
+		else if (iMenu == IN_NONE) {
+			break;
+		}
+
+		switch (iMenu)
+		{
+		case SM_WEAPON:
+			while (1)
+			{
+				system("cls");
+				Logo();
+				cout << endl;
+				cout << "--------------------방어구-------------------" << endl;
+				cout << endl;
+
+				for (int i = 0; i < weapon_count; i++) {
+						cout << "-----------------------------------------------" << endl;
+						cout << " " << "[" << i + 1 << setw(2) << "]" << setw(15) << weapons[i]->strName << setw(6) << "|" << setw(8) << weapons[i]->strTypeName << setw(5) << "|" << weapons[i]->iMin << "-" << weapons[i]->iMax << endl;
+					
+				}
+
+				cout << "장착할 무기의 번호를 입력해주세요[뒤로가기는 0번] : ";
+				cin >> iMenu;
+
+				if (cin.fail()) {
+					cin.clear();
+					cin.ignore(1024, '\n');
+					continue;
+				}
+				else if (iMenu == 0) {
+					break;
+				}
+
+				if (iMenu <= weapon_count && weapon_count>0) {
+					cout << weapons[iMenu - 1]->strName << "가 장착되었습니다!" << endl;
+					weapons[iMenu - 1]->strName = weapons[iMenu - 1]->strName + "[E]";
+					tPlayer->iEquipped[0] = weapons[iMenu-1];
+					system("pause");
+				}
+				else {
+					continue;
+				}
+
+			}
+			break;
+
+		case SM_ARMOR:
+			while (1)
+			{
+				system("cls");
+				Logo();
+				cout << endl;
+				cout << "--------------------방어구-------------------" << endl;
+				cout << endl;
+
+				for (int i = 0; i <armor_count; i++) {			
+						cout << "-----------------------------------------------" << endl;
+						cout << " " << "[" << i + 1 << setw(2) << "]" << setw(15) << armors[i]->strName << setw(6) << "|" << setw(8) << armors[i]->strTypeName << setw(5) << "|" << armors[i]->iMin << "-" << armors[i]->iMax << endl;
+				}
+
+				cout << "장착할 무기의 번호를 입력해주세요[뒤로가기는 0번] : ";
+				cin >> iMenu;
+
+				if (cin.fail()) {
+					cin.clear();
+					cin.ignore(1024, '\n');
+					continue;
+				}
+				else if (iMenu == 0) {
+					break;
+				}
+
+				if (iMenu <= armor_count && armor_count > 0) {
+					cout << armors[iMenu - 1]->strName << "가 장착되었습니다!" << endl;
+					armors[iMenu - 1]->strName = armors[iMenu - 1]->strName + "[E]";
+					tPlayer->iEquipped[1] = armors[iMenu - 1];
+					system("pause");
+				}
+				else {
+					continue;
+				}
+
+			}
+			break;
+
+		}
+		
+		
+	}
+}
+
+void Menu_UnEquip(_tagPlayer* tPlayer) {
+	int iMenu = 0;
+	
+	while (1) {
+		system("cls");
+		Logo();
+		cout << endl;
+		cout << "--------------------장비-------------------" << endl;
+		cout << endl;
+
+
+		if (tPlayer->iEquipped[0]!=nullptr) {
+			cout << "[무기]" << endl;
+			cout << setw(15) << tPlayer->iEquipped[0]->strName << setw(6) << "|" << setw(8) << tPlayer->iEquipped[0]->strTypeName << setw(5) << "|" << tPlayer->iEquipped[0]->iMin << "-" << tPlayer->iEquipped[0]->iMax << endl;
+		}
+		if (tPlayer->iEquipped[1] != nullptr) {
+			cout << "[방어구]" << endl;
+			cout << setw(15) << tPlayer->iEquipped[1]->strName << setw(6) << "|" << setw(8) << tPlayer->iEquipped[1]->strTypeName << setw(5) << "|" << tPlayer->iEquipped[1]->iMin << "-" << tPlayer->iEquipped[1]->iMax << endl;
+		}
+
+		cout << endl;
+		cout << "1. 무기" << endl;
+		cout << "2. 방어구" << endl;
+		cout << "해제할 장비의 번호를 입력해주세요 [뒤로가기는 0번]  : ";
+		
+		cin >> iMenu;
+
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(1024, '\n');
+			continue;
+		}
+		else if (iMenu ==SM_NONE) {
+			break;
+		}
+		
+		switch (iMenu)
+		{
+		case SM_WEAPON:
+			if (tPlayer->iEquipped[0]!= nullptr) {
+				string temp = tPlayer->iEquipped[0]->strName.substr(0, tPlayer->iEquipped[0]->strName.length() - 3);
+				tPlayer->iEquipped[0]->strName = temp;
+				cout << temp<< "이 장착해제 되었습니다." << endl;
+				tPlayer->iEquipped[0] = nullptr;
+			}
+			else {
+				cout << "장착된 무기가 없습니다." << endl;
+			}
+			system("pause");
+			break;
+		case SM_ARMOR:
+			if (tPlayer->iEquipped[1] != nullptr) {
+				string temp = tPlayer->iEquipped[1]->strName.substr(0, tPlayer->iEquipped[1]->strName.length() - 3);
+				tPlayer->iEquipped[1]->strName = temp;
+				cout << temp << "이 장착해제 되었습니다." << endl;
+				tPlayer->iEquipped[1] = nullptr;
+			}
+			else {
+				cout << "장착된 방어구가 없습니다." << endl;
+			}
+			system("pause");
+			break;
+		}
+		
+
+	}
+}
+
 void Menu_Inventory(_tagPlayer *tPlayer) {
 
 	int iMenu = 0;
@@ -294,7 +634,8 @@ void Menu_Inventory(_tagPlayer *tPlayer) {
 		cout << endl;
 
 		cout << endl;
-		cout << "1. 장착 모드" << endl;
+		cout << "1. 장착" << endl;
+		cout << "2. 장착 해제" << endl;
 		cout << "메뉴를 선택하세요 [뒤로가기는 0번]: ";
 		cin >> iMenu;
 
@@ -310,11 +651,10 @@ void Menu_Inventory(_tagPlayer *tPlayer) {
 		switch (iMenu)
 		{
 		case IN_EQUIP:
-			while (1) {
-				//장비를 보여주고 장착하는 기능 추가
-			}
+			Menu_Equip(tPlayer);
 			break;
-		default:
+		case IN_UNEQUIP:
+			Menu_UnEquip(tPlayer);
 			break;
 		}
 	}
@@ -663,7 +1003,7 @@ int main() {
 					Menu_Inventory(&tPlayer);
 					break;
 				case MM_STAT:
-					Show_Status(tPlayer);
+					Show_Status(&tPlayer);
 					break;
 				default:
 					cout << "잘못 선택하였습니다." << endl;
